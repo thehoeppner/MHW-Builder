b78a7da4e91c2c03955daa88d4e060ba58694f18
Lukas Höppner <lukashp46@gmail.com>
2018-09-05 00:55:14 +0200

    data

---
+++ MHWSkillCalc/Models/Charm/Charm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Charm
+{
+    public class Charm
+    {
+        public int id { get; set; }
+        public string slug { get; set; }
+        public string name { get; set; }
+        public IEnumerable<Rank> ranks { get; set; }
+    }
+}
---
+++ MHWSkillCalc/Models/Skill/SkillRankModifier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Skill
+{
+    public enum SkillRankModifier
+    {
+        Affinity,
+        Attack,
+        DamageFire,
+        DamageWater,
+        DamageIce,
+        DamageThunder,
+        DamageDragon,
+        Defense,
+        Health,
+        SharpnessBonus,
+        ResistAll,
+        ResistFire,
+        ResistWater,
+        ResistIce,
+        ResistThunder,
+        ResistDragon
+    }
+
+    public static class SkillRankModifierConverter
+    {
+    }
+}
